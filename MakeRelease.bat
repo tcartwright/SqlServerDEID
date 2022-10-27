ccbcf2135d8229fbf332e252ccf54b5deb08e75b
Tim Cartwright <tcartwright@users.noreply.github.com>
2022-10-27 09:26:21 -0400

    - added a "make" script

---
+++ MakeRelease.bat
@@ -0,0 +1,8 @@
+@rem bat file to ease use of the script
+
+@%~d0
+@cd "%~dp0"
+
+powershell.exe -ExecutionPolicy Bypass -NoLogo -NoProfile -file "%~dpn0.ps1" 
+
+@pause
--- MakeRelease.ps1
+++ MakeRelease.ps1
@@ -3,13 +3,16 @@ Clear-Host
 dotnet clean "$PSScriptRoot\SqlServerDEID.Editor\SqlServerDEID.Editor.csproj" -property:Configuration=Release 
 dotnet msbuild "$PSScriptRoot\SqlServerDEID.Editor\SqlServerDEID.Editor.csproj" -property:Configuration=Release 
 $buildPath = "$PSScriptRoot\SqlServerDEID.Editor\bin\Release\net4.8\"
+$releasePath = "$PSScriptRoot\SqlServerDEID.Editor\bin\Release\"
 
-Get-ChildItem $buildPath -Filter "Syncfusion*.dll" | Remove-Item -Verbose
+# Get-ChildItem $buildPath -Filter "Syncfusion*.dll" | Remove-Item -Verbose
 
 if (Test-Path "$buildPath\Testfiles" -PathType Container) {
     Remove-Item "$buildPath\Testfiles" -Force -Recurse 
 }
 
-Remove-Item "$buildPath\..\Release.zip" -Force -ErrorAction SilentlyContinue
-Get-ChildItem $buildPath | Compress-Archive -DestinationPath "$buildPath\..\Release.zip" -Update
+Remove-Item "$releasePath\SqlServerDEID.zip" -Force -ErrorAction SilentlyContinue
 
+Get-ChildItem $buildPath | Compress-Archive -DestinationPath "$releasePath\SqlServerDEID.zip" -Update
+
+. explorer.exe $releasePath
